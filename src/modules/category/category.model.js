4a4130e7826424a3e6eb2a0481aeb0e3167d16a9
Md Parvej <mdparvejme24@gmail.com>
2025-11-11 16:07:49 +0600

    added category model

---
+++ src/modules/category/category.model.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose");
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    slug: { type: String, required: true, unique: true },
+    image: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Category", CategorySchema);
